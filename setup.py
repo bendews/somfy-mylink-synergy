f49627e6cf97842e8fa0a2be334f6029569780db
Ben Dews <contact@bendews.com>
2019-03-28 14:31:45 +1100

    Bump version

--- setup.py
+++ setup.py
@@ -5,17 +5,17 @@ Install Somfy MyLink Synergy API
 from setuptools import setup, find_packages
 
 with open('README.md') as f:
-    long_description = f.read()
+    LONG_DESCRIPTION = f.read()
 
 setup(
     name='somfy_mylink_synergy',
-    version='1.0.3',
+    version='1.0.4',
     url='http://github.com/bendews/somfy-mylink-synergy',
     license='MIT',
     author='Ben Dews',
     author_email='contact@bendews.com',
     description='Python API to utilise the Somfy Synergy JsonRPC API',
-    long_description=long_description,
+    long_description=LONG_DESCRIPTION,
     long_description_content_type='text/markdown',
     packages=find_packages(),
     keywords='somfy mylink synergy covers sensors api jsonrpc',
