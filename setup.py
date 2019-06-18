3326d703b9a5e31d5d9f324caae6b1505698eb47
Ben Dews <contact@bendews.com>
2019-06-18 12:42:41 +1000

    Bump version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open('README.md') as f:
 
 setup(
     name='somfy_mylink_synergy',
-    version='1.0.4',
+    version='1.0.5',
     url='http://github.com/bendews/somfy-mylink-synergy',
     license='MIT',
     author='Ben Dews',
